ad16e8e685e83531ea1eb5abfab66040ec45349b
K6TD <kevin@rowett.org>
2015-06-29 12:53:51 -0700

    added 20dBm/100 mW case statement

--- beacon/rig.ino
+++ beacon/rig.ino
@@ -88,6 +88,10 @@ void setpower(byte dBm) {
       newPow = 2;
 	  FPPRINTRC(1,12,"30");
       break;
+    case 20:  // 20 dBm = 100 milliwatts
+      newPow = 2;
+	  FPPRINTRC(1,12,"30");
+      break;
     default:
       newPow = 1;
 	  FPPRINTRC(1,12,"20");
